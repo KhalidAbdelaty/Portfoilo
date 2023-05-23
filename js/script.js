ee597e6c1fdb97f9d83183ca3acfaeb1e5f18698
Khalid Mohamed Abdelaty <khalidabdelatty80@gmail.com>
2023-05-23 17:27:21 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -11,14 +10,14 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 
-window.onscroll =()=> {
-    sections.forEach( sec => {
+window.onscroll = () => {
+    sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top >=offset && top < offset + height ) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*= ' + id + ']').classList.add('active');
@@ -28,7 +27,7 @@ window.onscroll =()=> {
 
     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky',window.scrollY >100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
 
     menuIcon.classList.remove('bx-x');
@@ -38,23 +37,24 @@ window.onscroll =()=> {
 
 
 ScrollReveal({
-     //reset: true,
-     distance: '80px',
-     duration:2000,
-     delay:200
-    });
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
 
-    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-    ScrollReveal().reveal('.home-img, .services-container, .portfoilo-box, .contact form', { origin: 
-        'bottom' });
-        ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-        ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfoilo-box, .contact form', {
+    origin: 'bottom'
+});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
-        const typed = new Typed('.multiple-text', {
-            strings:['Frontend Developer','Data Analyst','Graraphic Designer'],
-            typeSpeed: 100,
-            backSpeed:100,
-            backDelay: 1000,
-            loop: true
-        });
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Data Analyst', 'Graraphic Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
