1bf91a62a6c9fcfd380f57b71329d499311d0517
Khalid Mohamed Abdelaty <khalidabdelatty80@gmail.com>
2023-08-05 17:15:00 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -60,18 +60,14 @@ const typed = new Typed('.multiple-text', {
 });
 
 /*======== swiper =======*/
-var swiper = new Swiper(".mySwiper", {
-
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
+var swiper = new Swiper('.swiper-container', {
+    grabCursor: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
     pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-
 });
