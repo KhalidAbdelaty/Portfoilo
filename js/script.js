5023ce2410578c042c0ad09e56ef803f2d595b25
Khalid Mohamed Abdelaty <khalidabdelatty80@gmail.com>
2024-02-15 00:49:34 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Data Analyst', 'Machine Learning Engineer'],
+    strings: ['Frontend Developer', 'Data Analyst', 'Machine Learning Trainee'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
